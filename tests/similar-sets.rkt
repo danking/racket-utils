b9483a323f75db42f64c5eba79749c83524ad188
Daniel King <danking@ccs.neu.edu>
2012-03-30 14:43:07 -0400

    add some headers to the tests

--- tests/similar-sets.rkt
+++ tests/similar-sets.rkt
@@ -3,6 +3,7 @@
          "../similar-sets.rkt")
 
 
+;; set-contains
 (check-true (let ((join (lambda (x y) x)))
               (set-contains/similar? (set-union (set join eq? eq? equal-hash-code 1 2 3)
                                                 (set join eq? eq? equal-hash-code 2 3 4))
@@ -28,6 +29,7 @@
                                                  (set join eq? eq? equal-hash-code 2 3 4))
                                       0)))
 
+;; set-contains -- more complex hash table procedures
 (check-true (set-contains/similar?
              (let ((join (lambda (x y) (max x y)))
                    (similar? (lambda (x y) (= (floor (/ x 10)) (floor (/ y 10)))))
@@ -49,6 +51,8 @@
                (set-union (set join eq? similar? hash-code 1 21 33)
                           (set join eq? similar? hash-code 25 32 47)))
              33))
+
+;; set-get-similar
 (check-eq? (set-get-similar
             (let ((join (lambda (x y) (max x y)))
                   (similar? (lambda (x y) (= (floor (/ x 10)) (floor (/ y 10)))))
