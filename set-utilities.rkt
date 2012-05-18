6602bf69ac76be2da9d66b6f89e6a1ca89b3aa3e
Daniel King <danking@ccs.neu.edu>
2012-05-18 17:11:51 -0400

    check that set-get-one/rest actually decreases the size of the set

--- set-utilities.rkt
+++ set-utilities.rkt
@@ -2,5 +2,16 @@
 (provide set-get-one/rest)
 
 (define (set-get-one/rest s)
-  (let ((e (for/first ((e (in-set s))) e)))
-    (values e (set-remove s e))))
+  (let* ((e (for/first ((e (in-set s))) e))
+         (s* (set-remove s e)))
+    (unless (equal? (set-count s*)
+                    (sub1 (set-count s)))
+      (printf "[error] old-set: ")
+      (pretty-print s)
+      (printf "[error] new-set: ")
+      (pretty-print s*)
+      (printf "[error] element: ")
+      (pretty-print e)
+      (error 'set-get-one/rest
+             "Removing an element from the set didn't change the set's size!?"))
+    (values e s*)))
