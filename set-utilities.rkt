16d534aa29b98a0d200be3d9a0499d97b3b8a1fe
Daniel King <danking@ccs.neu.edu>
2012-05-22 18:26:01 -0400

    add for/union for each set type

--- set-utilities.rkt
+++ set-utilities.rkt
@@ -1,6 +1,9 @@
 #lang racket
 (provide set-get-one/rest
-         get-set-constructor)
+         get-set-constructor
+         for/set-union
+         for/seteq-union
+         for/seteqv-union)
 
 (define (get-set-constructor s)
   (cond [(set-eq? s) seteq]
@@ -21,3 +24,16 @@
       (error 'set-get-one/rest
              "Removing an element from the set didn't change the set's size!?"))
     (values e s*)))
+
+(define-for-syntax (gen-for/set-union set-type)
+  (lambda (stx)
+   (syntax-case stx ()
+     [(_ clauses defs+exprs ...)
+      #`(for/fold/derived stx
+                          ([s (#,set-type)])
+                          clauses
+                          (set-union s
+                                     (begin defs+exprs ...)))])))
+(define-syntax for/set-union (gen-for/set-union 'set))
+(define-syntax for/seteq-union (gen-for/set-union 'seteq))
+(define-syntax for/seteqv-union (gen-for/set-union 'seteqv))
