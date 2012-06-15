a842a96b2d31fc7a4b7155b88f92cfe671bf2b51
Daniel King <danking@ccs.neu.edu>
2012-06-15 16:30:43 -0400

    support multiple body statements in a workset-loop

--- workset-loop.rkt
+++ workset-loop.rkt
@@ -2,26 +2,28 @@
 (require "set-utilities.rkt")
 (provide (all-defined-out))
 
+;;  - can I make current, workset, and value into match patterns?
+
 (define-syntax workset-loop!
   (syntax-rules ()
     ((_ initial-set (current workset)
-        body)
+        body ...)
      (let loop ((workset initial-set))
        (unless (set-empty? workset)
          (let*-values (((current workset) (set-get-one/rest workset))
-                       ((w) body))
+                       ((w) (let () body ...)))
            (loop w)))))))
 
 (define-syntax workset-loop
   (syntax-rules ()
     ((_ initial-set initial-value (current workset value)
-        body)
+        body ...)
      (let loop ((workset initial-set)
                 (value initial-value))
        (if (set-empty? workset)
            value
            (let*-values (((current workset) (set-get-one/rest workset))
-                         ((w v) body))
+                         ((w v) (let () body ...)))
              (loop w v)))))))
 
 
